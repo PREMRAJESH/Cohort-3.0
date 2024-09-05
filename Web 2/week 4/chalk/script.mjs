33103c089e362ee1be25738cd2d2572d6fcaf074
P.R.S <premraju943@gmail.com>
2024-09-06 01:40:01 +0530

    origin

---
+++ Web 2/week 4/chalk/script.mjs
@@ -0,0 +1,4 @@
+import chalk from "chalk";
+console.log(chalk.blueBright("Hello World"));
+console.log(chalk.red(chalk.bold("Error! on line 2")));
+console.log(chalk.green("Chalk Installed Sucessfully !!!"));
